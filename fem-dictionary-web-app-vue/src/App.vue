11b27f8f003bc614297bff26c39e1c4b8489970a
sinjin25 <raionus2@gmail.com>
2025-02-23 20:17:11 -0800

    start

---
+++ fem-dictionary-web-app-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Test from './components/Test.vue';
+</script>
+
+<template>
+    <div>
+        1231231231231
+        <Test></Test>
+    </div>
+</template>
+
+<style scoped></style>
