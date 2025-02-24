75c0e07e9c78263bc4adad5a97ce952290a855c2
sinjin25 <raionus2@gmail.com>
2025-02-24 03:24:15 -0800

    02-top

--- fem-dictionary-web-app-vue/src/App.vue
+++ fem-dictionary-web-app-vue/src/App.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import Test from './components/Test.vue';
+import Top from './components/Top.vue'
 </script>
 
 <template>
-    <div>
-        1231231231231
-        <Test></Test>
+    <div class="body">
+        <Top></Top>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.body {
+    // start with mobile size
+    width: 375px;
+    height: 1065px;
+
+    background: var(--white);
+
+    margin: auto;
+    padding: 24px 24px 63px 24px;
+}
+
+.--dark .body {
+    background: var(--b-1);
+}
+</style>
---
+++ fem-dictionary-web-app-vue/src/components/Top.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="d-flex jc-sb">
+        <IconLogo class="top-logo"></IconLogo>
+        <div class="top-right d-flex ai-c">
+            <div class="top-font-select">Mono</div>
+            <IconArrowDown class="top-arrow-down"></IconArrowDown>
+            <div class="top-border"></div>
+            <div class="top-switch d-flex">
+                <div class="top-switch-spacer-l"></div>
+                <div class="top-switch-knob"></div>
+                <div class="top-switch-spacer-r"></div>
+            </div>
+            <IconMoon class="top-moon"></IconMoon>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import IconLogo from './icon/Logo.vue'
+import IconArrowDown from './icon/ArrowDown.vue'
+import IconMoon from './icon/Moon.vue'
+</script>
+
+<style lang="scss" scoped>
+.top {
+    &-right {
+        gap: 16px;
+    }
+
+    &-logo {
+        width: 28px; //28x32
+        height: 32px;
+    }
+
+    &-font-select {
+        color: var(--b-3);
+    }
+
+    &-arrow-down {
+        width: 12px;
+        height: 8px;
+    }
+
+    &-border {
+        width: 1px;
+        height: 100%;
+        background: var(--gray-2);
+    }
+
+    &-switch {
+        padding: 3px;
+        min-width: 40px;
+        border-radius: 10px;
+
+        background: var(--gray-1);
+
+        &-knob {
+            width: 14px;
+            height: 14px;
+            border-radius: 50px;
+            background: var(--white);
+        }
+
+        &-spacer {
+            &-l {
+                flex: 0 0 1px;
+            }
+
+            &-r {
+                flex: 0 0 1px;
+            }
+        }
+    }
+
+    &-moon {
+        width: 20px;
+        height: 20px;
+        margin-left: -4px; // designs wants 12px between this and prev element for some reason
+        // this needs to be able to change color
+        stroke: var(--gray-1);
+    }
+}
+
+.--dark .top {
+    &-font-select {
+        color: var(--white);
+    }
+
+    &-switch {
+        background: var(--gray-1);
+
+        &-knob {
+            background: var(--white);
+        }
+    }
+}
+</style>
